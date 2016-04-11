68140d8c496e6ae75aa094d0b15b67680b0fdf0b
ngoix <goix.nicolas@gmail.com>
2016-04-11 20:07:57 +0200

    up

--- code/em.py
+++ code/em.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pdb
 
+n_generated = 500000
 
-def EM(estimator, X, t, lim_inf=None, lim_sup=None, precision=10,
-       n_generated=100000):
+
+def EM(estimator, X, t, lim_inf=None, lim_sup=None, n_generated=n_generated):
     '''
     Compute the mass-volume curve at point t of the scoring function
     corresponding to 'estimator'
@@ -24,27 +25,231 @@ def EM(estimator, X, t, lim_inf=None, lim_sup=None, precision=10,
     if lim_sup is None:
         lim_sup = X.max(axis=0)
     volume_support = (lim_sup - lim_inf).prod()
-    # pdb.set_trace()
     unif = np.random.uniform(lim_inf, lim_sup, size=(n_generated, X.shape[1]))
     s_unif = estimator.decision_function(unif)
     s_X = estimator.decision_function(X)  # the lower, the more abnormal
-    s_X_argsort = s_X.argsort()
+    # s_X_argsort = s_X.argsort()
+    # p = (n_samples - np.arange(n_samples)) / float(n_samples)
+    # Leb = (s_unif > s_X[s_X_argsort].reshape(-1, 1)).sum(
+    #     axis=1) * volume_support / n_generated
+    # EM_t = (p.reshape(-1, 1) - t * Leb.reshape(-1, 1)).max(axis=0)
 
-    p = (n_samples - np.arange(n_samples)) / float(n_samples)
-
-    s_unif_repeated = np.array([s_unif]).repeat(n_samples, axis=0)
-
-    Leb = (s_unif_repeated > s_X[s_X_argsort].reshape(-1, 1)).sum(
-        axis=1) / n_generated * volume_support
-    EM_t = (p.reshape(-1, 1) - t * Leb.reshape(-1, 1)).max(axis=0)
-    pdb.set_trace()
+    EM_t = np.zeros(t.shape[0])
+    min_s = min(s_unif.min(), s_X.min())
+    max_s = max(s_unif.max(), s_X.max())
+    for u in np.arange(min_s, max_s, (max_s - min_s) / 100):
+        if (s_unif >= u).sum() > 100:
+            EM_t = np.maximum(EM_t, 1. / n_samples * (s_X >= u).sum() -
+                              t * (s_unif >= u).sum() / n_generated * volume_support)
+    # pdb.set_trace()
     return EM_t
+
+
+def MV(estimator, X, alpha, lim_inf=None, lim_sup=None,
+       n_generated=n_generated):
+    '''
+    Compute the mass-volume curve at point t of the scoring function
+    corresponding to 'estimator'
+    Parameters:
+    estimator: fitted estimator (eg damex.predict)
+    X: testing data
+    t: float
+    lim_inf: numpy array of shape(d,) (default is None)
+        the infimum of the data support along each dimension
+        if None, computed wrt the testing data X only
+    lim_sup: numpy array of shape(d,) (default is None)
+        the supremum of the data support along each dimension
+        if None, computed wrt the testing data X only
+    '''
+    n_samples = X.shape[0]
+    if lim_inf is None:
+        lim_inf = X.min(axis=0)
+    if lim_sup is None:
+        lim_sup = X.max(axis=0)
+    volume_support = (lim_sup - lim_inf).prod()
+    unif = np.random.uniform(lim_inf, lim_sup, size=(n_generated, X.shape[1]))
+    s_unif = estimator.decision_function(unif)  # np.maximum(estimator.decision_function(unif), -1000)  # ACHTUNG!!
+    s_X = estimator.decision_function(X)  # np.maximum(estimator.decision_function(X), -1000)
+    s_X_argsort = s_X.argsort()
+    mass = 0
+    cpt = 0
+    u = s_X[s_X_argsort[-1]]
+    mv = np.zeros(alpha.shape[0])
+    for i in range(alpha.shape[0]):
+        # pdb.set_trace()
+        while mass < alpha[i]:
+            cpt += 1
+            u = s_X[s_X_argsort[-cpt]]
+            mass = 1. / n_samples * cpt  # sum(s_X > u)
+        mv[i] = float((s_unif >= u).sum()) / n_generated * volume_support
+    pdb.set_trace()
+    return mv
+
+    # s_X_argsort = s_X.argsort()
+    # max_s = s_unif.max()
+    # min_s = s_unif.min()
+    # u = min_s
+    # mass = 100
+    # cpt = -1
+    # mv = np.zeros(alpha.shape[0])
+    # for i in reversed(range(alpha.shape[0])):
+    #     # pdb.set_trace()
+    #     while mass > alpha[i]:
+    #         cpt += 1
+    #         u = s_X[s_X_argsort[cpt]]
+    #         # u -= (max_s - min_s) / precision
+    #         mass = 1. / n_samples * (n_samples - cpt - 1)  # sum(s_X > u)
+    #     mv[i] = float(sum(s_unif > u)) / n_generated * volume_support
+    # return mv
+
+
+def MV_approx(estimator, X, alpha, lim_inf=None, lim_sup=None,
+              precision=3):
+    '''
+    Compute an approximation of the mass-volume curve of the scoring function
+    corresponding to 'estimator'.
+    Parameters:
+    estimator: fitted estimator (eg damex.predict)
+    X: testing data
+    t: float
+    lim_inf: numpy array of shape(d,) (default is None)
+        the infimum of the data support along each dimension
+        if None, computed wrt the testing data X only
+    lim_sup: numpy array of shape(d,) (default is None)
+        the supremum of the data support along each dimension
+        if None, computed wrt the testing data X only
+    '''
+    n_samples, n_features = X.shape
+    if lim_inf is None:
+        lim_inf = X.min(axis=0) - 1e-7
+    # 1e-7 since otherwise (lim_sup - lim_inf - precision * step).max() > 0
+    # so that some data are not in any cell
+    if lim_sup is None:
+        lim_sup = X.max(axis=0) + 1e-7
+    feature_range = precision
+    step = (lim_sup - lim_inf) / precision
+    volume_cell = step.prod()
+    nb_cell = feature_range ** n_features
+    print 'nb_cell', nb_cell
+    n_samples_cell = np.zeros(nb_cell)
+    approx_scoring = np.zeros(nb_cell)
+    u_test = np.zeros((nb_cell, n_features))
+    print 'nb_cell=', nb_cell
+    print 'first loop with length nb_cell'
+    for i in range(nb_cell):
+        base_ = base(i, feature_range)
+        while len(base_) < n_features:
+            base_ = [0] + base_
+        inf = [lim_inf[j] + int(base_[j]) * step[j]
+               for j in range(n_features)]
+        sup = inf + step
+        index = (X <= sup).all(axis=1) * (X >= inf).all(axis=1)
+        n_samples_cell[i] = index.sum()
+        u_test[i] = inf + 0.5 * step
+    print 'prediction over the grid'
+    approx_scoring = estimator.decision_function(u_test)
+
+    if n_samples_cell.sum() != n_samples:
+        raise ValueError("some data are not in any cell"
+                         " need to increase 1e-7 in lim_inf and lim_sup")
+    print 'sorting the approx:'
+    a_s_argsort = approx_scoring.argsort()
+    print 'precomputing'
+    p = n_samples_cell[a_s_argsort[::-1]].cumsum(dtype=float) / n_samples
+    mv = np.argmax(p > alpha.reshape(-1, 1), axis=1) * volume_cell
+
+    # is equivalent to:
+    # for i in alpha:
+    #     mv[i] = np.argmax(p > i)
+    return mv
+
+
+def MV_approx_over(estimator, X, alpha, lim_inf=None, lim_sup=None,
+                   precision=2):
+
+    '''
+    Compute an approximation of mass-volume curve of the scoring function
+    corresponding to 'estimator'. Assume a null score on empty hypercube
+    wrt samples X.
+    (better result than the reality: under-estimate the MV curve).
+    (when precision is to large, overfit X -> weaker results than the reality)
+    Parameters:
+    estimator: fitted estimator (eg damex.predict)
+    X: testing data
+    t: float
+    lim_inf: numpy array of shape(d,) (default is None)
+        the infimum of the data support along each dimension
+        if None, computed wrt the testing data X only
+    lim_sup: numpy array of shape(d,) (default is None)
+        the supremum of the data support along each dimension
+        if None, computed wrt the testing data X only
+    '''
+    n_samples, n_features = X.shape
+    if lim_inf is None:
+        lim_inf = X.min(axis=0) - 1e-7
+    # 1e-7 since otherwise (lim_sup - lim_inf - precision * step).max() > 0
+    # so that some data are not in any cell
+    if lim_sup is None:
+        lim_sup = X.max(axis=0) + 1e-7
+    feature_range = precision
+    step = (lim_sup - lim_inf) / precision
+    volume_cell = step.prod()
+    nb_cell = feature_range ** n_features
+    print 'nb_cell=', nb_cell
+
+    decision_func_X = estimator.decision_function(X)
+    cell_dict = {}
+    for i in range(n_samples):
+        x = X[i, :]
+        y = decision_func_X[i]
+        base_ = np.array((x - lim_inf) / step, dtype=int)
+        num_cell = nombre(base_, n_features)
+        if num_cell in cell_dict:
+            cell_dict[num_cell] += np.array([1, y])
+        else:
+            cell_dict[num_cell] = np.array([1, y])
+
+    approx_scoring = np.array([cell_dict[k][1] / cell_dict[k][0]
+                               for k in cell_dict])
+    n_samples_cell = np.array([cell_dict[k][0] for k in cell_dict])
+
+    if n_samples_cell.sum() != n_samples:
+        raise ValueError("some data are not in any cell"
+                         " need to increase 1e-7 in lim_inf and lim_sup")
+    print 'sorting the approx:'
+    a_s_argsort = approx_scoring.argsort()
+    print 'precomputing'
+    p = n_samples_cell[a_s_argsort[::-1]].cumsum(dtype=float) / n_samples
+    mv = np.argmax(p > alpha.reshape(-1, 1), axis=1) * volume_cell
+
+    # is equivalent to:
+    # for i in alpha:
+    #     mv[i] = np.argmax(p > i)
+    return mv
+
+    # could just return the tuple (np.arange(1, nb_non_empty_cell + 1), p)
+
+
+def base(n, b):
+    if n == 0:
+        return []
+    else:
+        return base(n // b, b) + [n % b]
+
+
+def nombre(a, b):
+    n = 0
+    cpt = 0
+    for j in range(a.shape[0]):
+        cpt += 1
+        n += a[j] * b ** cpt
+    return n
+
+
+####### EM ########################################
 
     # EM_t = np.array([1. / n_samples * (n_samples - i) - t * sum(s_unif > s_X[s_X_argsort[i]]) / n_generated * volume_support for i in range(n_samples)]).max(axis=0)
     # EM_t = np.maximum(np.zeros(t.shape[0]), EM_t)
-
-
-
 
     # n_samples = X.shape[0]
     # if lim_inf is None:
@@ -67,10 +272,70 @@ def EM(estimator, X, t, lim_inf=None, lim_sup=None, precision=10,
     # return EM_t
 
 
-def EM_approx(estimator, X, t, lim_inf=None, lim_sup=None, precision=70):
+def EM_approx(estimator, X, t, lim_inf=None, lim_sup=None, precision=2):
     '''
-    Compute an approximation of the mass-volume curve at point t of the scoring
-    function corresponding to 'estimator'
+    Compute an approximation of excess-masss curve of the scoring function
+    corresponding to 'estimator'.
+    Parameters:
+    estimator: fitted estimator (eg damex.predict)
+    X: testing data
+    t: array of float
+    lim_inf: numpy array of shape(d,) (default is None)
+        the infimum of the data support along each dimension
+        if None, computed wrt the testing data X only
+    lim_sup: numpy array of shape(d,) (default is None)
+        the supremum of the data support along each dimension
+        if None, computed wrt the testing data X only
+    '''
+    n_samples, n_features = X.shape
+    if lim_inf is None:
+        lim_inf = X.min(axis=0) - 1e-7
+    # 1e-7 since otherwise (lim_sup - lim_inf - precision * step).max() > 0
+    # so that some data are not in any cell
+    if lim_sup is None:
+        lim_sup = X.max(axis=0) + 1e-7
+    feature_range = precision
+    step = (lim_sup - lim_inf) / precision
+    volume_cell = step.prod()
+    nb_cell = feature_range ** n_features
+    n_samples_cell = np.zeros(nb_cell)
+    approx_scoring = np.zeros(nb_cell)
+    u_test = np.zeros((nb_cell, n_features))
+    print 'nb_cell=', nb_cell
+    print 'first loop with length nb_cell'
+    for i in range(nb_cell):
+        base_ = base(i, feature_range)
+        while len(base_) < n_features:
+            base_ = [0] + base_
+        inf = [lim_inf[j] + int(base_[j]) * step[j]
+               for j in range(n_features)]
+        sup = inf + step
+        index = (X <= sup).all(axis=1) * (X >= inf).all(axis=1)
+        n_samples_cell[i] = index.sum()
+        u_test[i] = inf + 0.5 * step
+    print 'prediction over the grid'
+    approx_scoring = estimator.decision_function(u_test)
+
+    if n_samples_cell.sum() != n_samples:
+        raise ValueError("some data are not in any cell"
+                         " need to increase 1e-7 in lim_inf and lim_sup")
+    print 'sorting the approx:'
+    a_s_argsort = approx_scoring.argsort()
+    print 'precomputing'
+    p = n_samples_cell[a_s_argsort[::-1]].cumsum(dtype=float) / n_samples
+    print 'computing EM avoiding for loop'
+    EM_t = np.array([p[i] - t * volume_cell * i
+                     for i in range(0, nb_cell)]).max(axis=0)
+    print 'last operation'
+    EM_t = np.maximum(np.zeros(t.shape[0]), EM_t)
+    return EM_t
+
+
+def EM_approx_over(estimator, X, t, lim_inf=None, lim_sup=None, precision=100):
+    '''
+    Compute an approximation of excess-masss curve of the scoring function
+    corresponding to 'estimator'. Assume a null score on empty hypercube
+    wrt samples X.
     Parameters:
     estimator: fitted estimator (eg damex.predict)
     X: testing data
@@ -94,8 +359,6 @@ def EM_approx(estimator, X, t, lim_inf=None, lim_sup=None, precision=70):
     volume_cell = step.prod()
     nb_cell = feature_range ** n_features
     print 'nb_cell=', nb_cell
-    approx_scoring = np.zeros(nb_cell)
-    n_samples_cell = np.zeros(nb_cell)
     print 'first loop with length nb_cell'
 
     decision_func_X = estimator.decision_function(X)
@@ -121,6 +384,7 @@ def EM_approx(estimator, X, t, lim_inf=None, lim_sup=None, precision=70):
     a_s_argsort = approx_scoring.argsort()
     print 'precomputing'
     p = n_samples_cell[a_s_argsort[::-1]].cumsum(dtype=float) / n_samples
+    p = np.r_[[0], p]  # adding an empty cell
     print 'computing EM avoiding for loop'
     EM_t = np.array([p[i] - t * volume_cell * i
                      for i in range(0, nb_non_empty_cell)]).max(axis=0)
@@ -177,78 +441,3 @@ def EM_approx(estimator, X, t, lim_inf=None, lim_sup=None, precision=70):
     #         else:
     #             EM_t[i] = EM_tmp
     #             p = p_tmp
-
-
-def MV(estimator, X, alpha, lim_inf=None, lim_sup=None, precision=1000,
-       n_generated=10000):
-    '''
-    Compute the mass-volume curve at point t of the scoring function
-    corresponding to 'estimator'
-    Parameters:
-    estimator: fitted estimator (eg damex.predict)
-    X: testing data
-    t: float
-    lim_inf: numpy array of shape(d,) (default is None)
-        the infimum of the data support along each dimension
-        if None, computed wrt the testing data X only
-    lim_sup: numpy array of shape(d,) (default is None)
-        the supremum of the data support along each dimension
-        if None, computed wrt the testing data X only
-    '''
-    n_samples = X.shape[0]
-    if lim_inf is None:
-        lim_inf = X.min(axis=0)
-    if lim_sup is None:
-        lim_sup = X.max(axis=0)
-    volume_support = (lim_sup - lim_inf).prod()
-    unif = np.random.uniform(lim_inf, lim_sup, size=(n_generated, X.shape[1]))
-    s_unif = np.maximum(estimator.decision_function(unif), -1000)
-    s_X = np.maximum(estimator.decision_function(X), -1000)
-    s_X_argsort = s_X.argsort()
-    mass = 0
-    cpt = 0
-    MV = np.zeros(alpha.shape[0])
-    for i in range(alpha.shape[0]):
-        # pdb.set_trace()
-        while mass < alpha[i]:
-            print i, mass, alpha[i]
-            cpt += 1
-            u = s_X[s_X_argsort[-cpt]]
-            # u -= (max_s - min_s) / precision
-            mass = 1. / n_samples * cpt  # sum(s_X > u)
-        MV[i] = float(sum(s_unif > u)) / n_generated * volume_support
-    return MV
-
-    # s_X_argsort = s_X.argsort()
-    # max_s = s_unif.max()
-    # min_s = s_unif.min()
-    # u = min_s
-    # mass = 100
-    # cpt = -1
-    # MV = np.zeros(alpha.shape[0])
-    # for i in reversed(range(alpha.shape[0])):
-    #     # pdb.set_trace()
-    #     while mass > alpha[i]:
-    #         print i, mass, alpha[i]
-    #         cpt += 1
-    #         u = s_X[s_X_argsort[cpt]]
-    #         # u -= (max_s - min_s) / precision
-    #         mass = 1. / n_samples * (n_samples - cpt - 1)  # sum(s_X > u)
-    #     MV[i] = float(sum(s_unif > u)) / n_generated * volume_support
-    # return MV
-
-
-def base(n, b):
-    if n == 0:
-        return []
-    else:
-        return base(n // b, b) + [n % b]
-
-    
-def nombre(a, b):
-    n = 0
-    cpt = 0
-    for j in range(a.shape[0]):
-        cpt += 1
-        n += a[j] * b ** cpt
-    return n
--- code/em_bench.py
+++ code/em_bench.py
@@ -1,25 +1,167 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import pdb
+# import matplotlib.pyplot as plt
+# for the cluster to save the fig:
+import sys
+sys.path.insert(1, '/home/nicolas/Bureau/OCRF')
+
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
 from sklearn.neighbors import LocalOutlierFactor
+from sklearn.svm import OneClassSVM
 from sklearn.ensemble import IsolationForest
 from sklearn.metrics import auc
-from sklearn.datasets import fetch_kddcup99
-from em import EM, MV, EM_approx
+from sklearn.utils import shuffle as sh
+from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_mldata
+from sklearn.datasets import fetch_spambase, fetch_annthyroid, fetch_arrhythmia
+from sklearn.datasets import fetch_pendigits, fetch_pima, fetch_wilt
+from sklearn.datasets import fetch_internet_ads, fetch_adult
+from em import EM, MV  # , EM_approx, MV_approx, MV_approx_over
+from sklearn.preprocessing import LabelBinarizer
+
 
 np.random.seed(1)
 
+# TODO: find good default parameters for every datasets
+# TODO: make an average of ROC curves over 10 experiments
+# TODO: idem in bench_lof, bench_isolation_forest (to be launch from master)
+#       bench_ocsvm (to be created), bench_ocrf (to be created)
 
-datasets = ['http']  # , 'http']
+# # datasets available:
+# datasets = ['http', 'smtp', 'SA', 'SF', 'shuttle', 'forestcover',
+#             'ionosphere', 'spambase', 'annthyroid', 'arrhythmia',
+#             'pendigits', 'pima', 'wilt',  # 'internet_ads',
+#             'adult']
+
+# # continuous datasets:
+# datasets = ['http', 'smtp', 'shuttle', 'forestcover',
+#             'ionosphere', 'spambase', 'annthyroid', 'arrhythmia',
+#             'pendigits', 'pima', 'wilt', 'adult']
+# new: ['ionosphere', 'spambase', 'annthyroid', 'arrhythmia', 'pendigits',
+#       'pima', 'wilt', 'adult']
+
+datasets = ['http', 'smtp', 'shuttle', # 'spambase',
+            'pendigits', 'pima', 'wilt', 'adult']
+# datasets = ['shuttle']
+plt.figure(figsize=(25, 13))
 
 for dat in datasets:
+    print 'dataset:', dat
     # loading and vectorization
     print('loading data')
-    if dat in ['http', 'smtp', 'SA', 'SF']:
-        dataset = fetch_kddcup99(subset=dat, shuffle=True, percent10=True)
+
+    if dat == 'adult':
+        dataset = fetch_adult(shuffle=True)
+        X = dataset.data
+        y = dataset.target
+        # anormal data are those with label >50K:
+        y = np.all((y != ' <=50K', y != ' <=50K.'), axis=0).astype(int)
+
+    if dat == 'internet_ads':  # not adapted to oneclassrf
+        dataset = fetch_internet_ads(shuffle=True)
+        X = dataset.data
+        y = dataset.target
+        y = (y == 'ad.').astype(int)
+
+    if dat == 'wilt':
+        dataset = fetch_wilt(shuffle=True)
+        X = dataset.data
+        y = dataset.target
+        y = (y == 'w').astype(int)
+
+    if dat == 'pima':
+        dataset = fetch_pima(shuffle=True)
         X = dataset.data
         y = dataset.target
 
-    y = (y != 'normal.').astype(int)
+    if dat == 'pendigits':
+        dataset = fetch_pendigits(shuffle=True)
+        X = dataset.data
+        y = dataset.target
+        y = (y == 4).astype(int)
+        # anomalies = class 4
+
+    if dat == 'arrhythmia':
+        dataset = fetch_arrhythmia(shuffle=True)
+        X = dataset.data
+        y = dataset.target
+        # rm 5 features containing some '?' (XXX to be mentionned in paper)
+        X = np.delete(X, [10, 11, 12, 13, 14], axis=1)
+        y = (y != 1).astype(int)
+        # normal data are then those of class 1
+
+    if dat == 'annthyroid':
+        dataset = fetch_annthyroid(shuffle=True)
+        X = dataset.data
+        y = dataset.target
+        y = (y != 3).astype(int)
+        # normal data are then those of class 3
+
+    if dat == 'spambase':
+        dataset = fetch_spambase(shuffle=True)
+        X = dataset.data
+        y = dataset.target
+
+    if dat == 'ionosphere':
+        dataset = fetch_mldata('ionosphere')
+        X = dataset.data
+        y = dataset.target
+        X, y = sh(X, y)
+        y = (y != 1).astype(int)
+
+    if dat in ['http', 'smtp', 'SA', 'SF']:
+        dataset = fetch_kddcup99(subset=dat, shuffle=True, percent10=False)
+        X = dataset.data
+        y = dataset.target
+
+    if dat == 'shuttle':
+        dataset = fetch_mldata('shuttle')
+        X = dataset.data
+        y = dataset.target
+        X, y = sh(X, y)
+        # we remove data with label 4
+        # normal data are then those of class 1
+        s = (y != 4)
+        X = X[s, :]
+        y = y[s]
+        y = (y != 1).astype(int)
+
+    if dat == 'forestcover':
+        dataset = fetch_covtype(shuffle=True)
+        X = dataset.data
+        y = dataset.target
+        # normal data are those with attribute 2
+        # abnormal those with attribute 4
+        s = (y == 2) + (y == 4)
+        X = X[s, :]
+        y = y[s]
+        y = (y != 2).astype(int)
+
+    print('vectorizing data')
+
+    if dat == 'SF':
+        lb = LabelBinarizer()
+        lb.fit(X[:, 1])
+        x1 = lb.transform(X[:, 1])
+        X = np.c_[X[:, :1], x1, X[:, 2:]]
+        y = (y != 'normal.').astype(int)
+
+    if dat == 'SA':
+        lb = LabelBinarizer()
+        lb.fit(X[:, 1])
+        x1 = lb.transform(X[:, 1])
+        lb.fit(X[:, 2])
+        x2 = lb.transform(X[:, 2])
+        lb.fit(X[:, 3])
+        x3 = lb.transform(X[:, 3])
+        X = np.c_[X[:, :1], x1, x2, x3, X[:, 4:]]
+        y = (y != 'normal.').astype(int)
+
+    if dat == 'http' or dat == 'smtp':
+        y = (y != 'normal.').astype(int)
 
     n_samples, n_features = np.shape(X)
     n_samples_train = n_samples // 2
@@ -31,54 +173,82 @@ for dat in datasets:
     y_train = y[:n_samples_train]
     y_test = y[n_samples_train:]
 
-    axis_t = np.arange(0, 10, 0.01)
-    axis_alpha = np.arange(0, 1, 0.01)
+    volume_support = (X_test.max(axis=0) - X_test.min(axis=0)).prod()
+
+    axis_t = np.arange(0, 100. / volume_support, 0.01 / volume_support)
+    axis_alpha = np.arange(0.9, 0.99, 0.001)
 
     # fit:
-    print('LocalOutlierFactor processing...')
-    lof = LocalOutlierFactor(n_neighbors=20)
-    lof.fit(X_train)
-
     print('IsolationForest processing...')
     iforest = IsolationForest()
     iforest.fit(X_train)
+    print('LocalOutlierFactor processing...')
+    lof = LocalOutlierFactor(n_neighbors=20)
+    lof.fit(X_train)
+    print('OneClassSVM processing...')
+    ocsvm = OneClassSVM()
+    ocsvm.fit(X_train)
 
     # EM:
-    plt.subplot(121)
-    # em_lof = EM_approx(lof, X_test, axis_t)
-    # AUC = auc(axis_t, em_lof)
-    # plt.plot(axis_t, em_lof, lw=1, label='EM-curve of %s for %s (area = %0.3f)'
-    #          % ('lof', dat, AUC))
-    print 'em_iforest...'
+    print 'em_iforest'
     em_iforest = EM(iforest, X_test, axis_t)
-    AUC = auc(axis_t, em_iforest)
-    plt.plot(axis_t, em_iforest, lw=1,
-             label='EM-curve of %s for %s (area = %0.3f)'
-             % ('iforest', dat, AUC))
+    amax_iforest = np.argmax(em_iforest <= 0.9)
+    pdb.set_trace()
+    AUC_iforest = auc(axis_t[:amax_iforest], em_iforest[:amax_iforest])
+    print 'em_lof'
+    em_lof = EM(lof, X_test, axis_t)
+    amax_lof = np.argmax(em_lof <= 0.9)
+    AUC_lof = auc(axis_t[:amax_lof], em_lof[:amax_lof])
+    print 'em_ocsvm'
+    em_ocsvm = EM(ocsvm, X_test, axis_t)
+    amax_ocsvm = np.argmax(em_ocsvm <= 0.9)
+    AUC_ocsvm = auc(axis_t[:amax_ocsvm], em_ocsvm[:amax_ocsvm])
+
+    plt.subplot(121)
+    amax = max(amax_iforest, amax_lof, amax_ocsvm)
+    plt.plot(axis_t[:amax], em_iforest[:amax], lw=1,
+             label='%s for %s dataset (em_score = %0.3e)'
+             % ('iforest', dat, AUC_iforest))
+    plt.plot(axis_t[:amax], em_lof[:amax], lw=1,
+             label='%s for %s (area = %0.3e)'
+             % ('lof', dat, AUC_lof))
+    plt.plot(axis_t[:amax], em_ocsvm[:amax], lw=1,
+             label='%s for %s (area = %0.3e)'
+             % ('ocsvm', dat, AUC_ocsvm))
+
     # plt.xlim([-0.05, 1.05])
-    # plt.ylim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
     plt.xlabel('t')
     plt.ylabel('EM(t)')
     plt.title('Excess-Mass curve')
     plt.legend(loc="lower right")
 
-    # # MV:
-    # plt.subplot(122)
-    # mv_lof = MV(lof, X_test, axis_alpha)
-    # AUC = auc(axis_alpha, mv_lof)
-    # plt.plot(axis_alpha, mv_lof, lw=1,
-    #          label='MV-curve of %s for %s (area = %0.3f)'
-    #          % ('lof', dat, AUC))
+    # MV:
+    plt.subplot(122)
+    print 'mv_iforest'
+    mv_iforest = MV(iforest, X_test, axis_alpha)
+    AUC = auc(axis_alpha, mv_iforest)
+    plt.plot(axis_alpha, mv_iforest, lw=1,
+             label='%s (area = %0.3e)'
+             % ('iforest', dat, AUC))
+    print 'mv_lof'
+    mv_lof = MV(lof, X_test, axis_alpha)
+    AUC = auc(axis_alpha, mv_lof)
+    plt.plot(axis_alpha, mv_lof, lw=1,
+             label='%s (area = %0.3e)'
+             % ('lof', dat, AUC))
+    print 'mv_ocsvm'
+    mv_ocsvm = MV(ocsvm, X_test, axis_alpha)
+    AUC = auc(axis_alpha, mv_ocsvm)
+    plt.plot(axis_alpha, mv_ocsvm, lw=1,
+             label='%s (area = %0.3e)'
+             % ('ocsvm', dat, AUC))
 
-    # mv_iforest = MV(iforest, X_test, axis_alpha)
-    # AUC = auc(axis_alpha, mv_iforest)
-    # plt.plot(axis_alpha, mv_iforest, lw=1,
-    #          label='MV-curve of %s for %s (area = %0.3f)'
-    #          % ('iforest', dat, AUC))
-    # plt.xlim([-0.05, 1.05])
-    # plt.xlabel('alha')
-    # plt.ylabel('MV(alpha)')
-    # plt.title('Mass-Volume Curve')
-    # plt.legend(loc="lower right")
+    plt.xlim([-0.05, 1.05])
+    # plt.ylim([-0.05, 100])
+    plt.xlabel('alpha')
+    plt.ylabel('MV(alpha)')
+    plt.title('Mass-Volume Curve for' + dat)
+    plt.legend(loc="upper left")
 
-plt.show()
+    plt.savefig('mv_em' + dat)
